8ecb1a4fc43290b1f2d45340974ac7e2fdb2419b
junhub98 <t2j4fq488j@privaterelay.appleid.com>
2025-12-26 07:27:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 372 ms, Memory: 93488 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+
+const input = fs.readFileSync(0, 'utf8').toString().trim().split("\n");
+
+const length = Number(input[0]);
+
+
+const arr = input[1].split(" ").map(Number);
+
+
+
+
+
+console.log(Math.min(...arr)+" "+Math.max(...arr));
